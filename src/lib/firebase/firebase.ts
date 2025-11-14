2b27b125ec33c5c72828f3f3afabebe5fc0d135a
Pauline Lago <lagopauline28@gmail.com>
2025-11-14 15:45:36 +0000

    FirebaseError: Expected first argument to collection() to be a Collectio

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,10 +1,8 @@
-"use client";
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "./config";
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { app, db };
